985fef9498e5740e3ed0afbc45247beb4c93dc8d
katherine <muedasnka@gmail.com>
2019-03-19 15:13:17 -0400

    initialize an instance variable @list

--- prime_number_engine.rb
+++ prime_number_engine.rb
@@ -1,23 +1,21 @@
 class PrimeNumberEngine
   def initialize(limit)
     @limit = limit
-  end
-
-  def number_list
-    (2..@limit).to_a
+    @list = (2..@limit).to_a
   end
 
   def cross_out_multiple_of_two
-    number_list.reject { |n| n % 2 == 0 unless n == 2}
+    @list.reject { |n| n % 2 == 0 unless n == 2}
   end
 
   def cross_out_multiple_of_three
-    list = cross_out_multiple_of_two
-    list.reject { |n| n % 3 == 0 unless n == 3 }
+    @list = cross_out_multiple_of_two
+    @list.reject { |n| n % 3 == 0 unless n == 3 }
   end
 
   def cross_out_multiple_of_five
-    list = cross_out_multiple_of_three
-    list.reject { |n| n % 5 == 0 unless n == 5 }
+    @list = cross_out_multiple_of_three
+    @list.reject { |n| n % 5 == 0 unless n == 5 }
   end
+
 end
